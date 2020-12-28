7b8fef9692b92f1925697f3ae472fa956f57c86b
Jenia22f <ye.altukhov@gmail.com>
2020-12-28 14:25:16 +0200

    added worked code (with errors)

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const fs = require("fs");
-
-
+const winston = require('winston');
 
 let siteQuery = fs.readFileSync("input.txt", "utf8");
 
@@ -13,65 +12,30 @@ for (let i of siteQuery) {
   if (i.match(/^https:\/\//)) URL = i;
   else URL = 'https://' + i + '/';
 
+  // настройка логера winston
+  const logger = winston.createLogger({
+    level: 'error',
+    format: winston.format.json(),
+    defaultMeta: { service: URL },
+    transports: [
+      //
+      // - Write all logs with level `error` and below to `error.log`
+      // - Write all logs with level `info` and below to `combined.log`
+      //
+      new winston.transports.File({ filename: 'js_console_errors.log', level: 'error' }),
+      // new winston.transports.File({ filename: 'combined.log' }),
+    ],
+  });
+
+  // добавляем в очередь страницу thanks.php
+  processUrl(`${URL}thanks.php`);
+
   (async function example() {
     processUrl(URL);
-    // let driver = await new Builder().forBrowser('chrome').build();
-    // try {
-    //   await driver.get(URL);
-    //   driver.sleep(10000);
-
-    //   let resultObj = {};
-    //   let errors = await driver.manage().logs().get('browser');
-
-
-
-    //   if (errors.length === 0) return;
-
-    //   resultObj[URL]= {};
-    //   resultObj[URL].errors = [];
-    //   for (let [i, err] of errors.entries()) {
-    //     let obj = new Object(err);
-    //     // если нужно скипать предупреждение
-    //     if (obj.level.name_ === 'WARNING') continue;
-    //     resultObj[URL].errors.push(obj);
-    //   }
-  
-    //   // ничего не делаем если нет ошибок
-    //   if (resultObj[URL].errors.length === 0) return;
-
-    //   let myJSON;
-      
-    //   fs.readFile('output.json', 'utf8', function readFileCallback(err, data){
-    //     if (err){
-    //         console.log(err);
-    //     } else {
-    //       myJSON = JSON.parse(data); //now it an object 
-    //       myJSON.sites.push(resultObj);
-    //       myJSON = JSON.stringify(myJSON, null, 4);
-  
-    //       fs.writeFile('output.json', myJSON, function(error){
-    //         if(error) throw error; // если возникла ошибка
-                        
-    //         console.log('Запись файла завершена.');
-    //       });
-    //   }});
-      
-      
-  
-    //   // await driver.manage().logs().get("browser")
-    //   // .then(function(entries) {
-    //   //    entries.forEach(function(entry) {
-    //   //      console.log('[%s] %s', entry.level.name, entry.message);
-    //   //    });
-    //   // });
-      
-    // } finally {
-    //   await driver.quit();
-    // }
   })();
 
   async function processUrl(URL) {
-    console.log(URL);
+    console.log('start: ', URL);
     let driver = await new Builder().forBrowser('chrome').build();
     try {
       await driver.get(URL);
@@ -87,16 +51,16 @@ for (let i of siteQuery) {
         resultObj[URL].errors = [];
         for (let [i, err] of errors.entries()) {
           let obj = new Object(err);
-          // если нужно скипать предупреждение
-          if (obj.level.name_ === 'WARNING') continue;
+          // если нужно скипать WARNING ошибки
+          // if (obj.level.name_ === 'WARNING') continue;
           resultObj[URL].errors.push(obj);
         }
     
-        // продолжаем если приоритет ошибок больше не WARNING
+        // продолжаем если приоритет ошибок больше WARNING
         if (resultObj[URL].errors.length !== 0) {
           let myJSON;
         
-          fs.readFile('output.json', 'utf8', function readFileCallback(err, data){
+          fs.readFile('web_console_errors.json', 'utf8', function readFileCallback(err, data){
             if (err){
                 console.log(err);
             } else {
@@ -104,7 +68,7 @@ for (let i of siteQuery) {
               myJSON.sites.push(resultObj);
               myJSON = JSON.stringify(myJSON, null, 4);
       
-              fs.writeFile('output.json', myJSON, function(error){
+              fs.writeFile('web_console_errors.json', myJSON, function(error){
                 if(error) throw error; // если возникла ошибка
                             
                 console.log('Запись файла завершена.');
@@ -117,21 +81,15 @@ for (let i of siteQuery) {
       let links = await driver.findElements(By.css('a'));
       for(let link of links) {
         let href = await link.getAttribute('href');
-        // console.log(href);
         if (href === null) continue;
         else if (href === URL) continue;
         else if (href.match(URL)) processUrl(href);
-        // console.log(href);
-        // console.log(await link.getAttribute('href'));
       }
-  
-      // await driver.manage().logs().get("browser")
-      // .then(function(entries) {
-      //    entries.forEach(function(entry) {
-      //      console.log('[%s] %s', entry.level.name, entry.message);
-      //    });
-      // });
-      
+    } catch (e) {
+      logger.log({
+        level: 'error',
+        message: e.message
+      });
     } finally {
       await driver.quit();
     }
